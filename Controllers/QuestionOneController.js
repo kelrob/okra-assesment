09406b1959860f49304b3a16477501e304803ce9
Robert Ebafua <robertebafua@gmail.com>
2021-09-15 16:53:06 +0100

    Question One Solution

--- Controllers/QuestionOneController.js
+++ Controllers/QuestionOneController.js
@@ -2,6 +2,11 @@ const _ = require("lodash");
 
 const QuestionOneController = {};
 
+QuestionOneController.index = (req, res) => {
+  _.times(3, connectApp);
+  res.json(_.times(3, connectApp));
+};
+
 let app = {
   id: "484929849",
   company_name: "Budget Core Limited",
@@ -29,28 +34,28 @@ let user = {
   ],
 };
 
-QuestionOneController.index = (req, res) => {
-  _.times(3, connectApp);
-
-  console.log(
-    "User => ",
-    { id: user.id, name: user.name },
-    "\nAccounts  =>",
-    user.accounts
-  );
-};
-
 function connectApp() {
+  // New Account List
   let new_accounts = [];
 
+  // Loop through all Current Apps to add to new account list
   user.accounts.map((account) => {
     let new_account = JSON.parse(JSON.stringify(account));
-    if (account.connected) {
-      new_accounts.push(new_account);
-      account.connected_apps.push(app.slug);
+
+    new_accounts.push(new_account);
+
+    if (account.connected === true) {
+      // Loop through connected apps and make sure it is added just once
+      new_accounts[0].connected_apps.forEach((connected_app) => {
+        if (new_accounts[0].connected_apps.indexOf(app.slug) === -1) {
+          new_accounts[0].connected_apps.push(app.slug);
+        }
+      });
     }
   });
+
   user.accounts = new_accounts;
+  return new_accounts;
 }
 
 module.exports = QuestionOneController;
